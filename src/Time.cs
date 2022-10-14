43d5e02345d48c12048ebda17310aa5caa2109e2
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2022-10-14 18:35:41 -0500

    Updates

---
+++ src/Time.cs
@@ -0,0 +1,95 @@
+﻿//******************************************************************************************************
+//  Time.cs - Gbtc
+//
+//  Copyright © 2022, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/20/2022 - Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+using Gemstone;
+
+namespace sttp;
+
+/// <summary>
+/// Defines constants and functions for tick values, 64-bit integers used to designate time in STTP.
+/// </summary>
+/// <remarks>
+/// A tick value represents the number of 100-nanosecond intervals that have elapsed since 12:00:00
+/// midnight, January 1, 0001 UTC, Gregorian calendar. A single tick represents one hundred nanoseconds,
+/// or one ten-millionth of a second. There are 10,000 ticks in a millisecond and 10 million ticks in
+/// a second. Only bits 01 to 62 (0x3FFFFFFFFFFFFFFF) are used to represent the timestamp value.
+/// Bit 64 (0x8000000000000000) is used to denote leap second, i.e., second 60, where actual second
+/// value would remain at 59. Bit 63 (0x4000000000000000) is used to denote leap second direction,
+/// 0 for add, 1 for delete.
+/// </remarks>
+public static class Time
+{
+    /// <summary>
+    /// Defines the flag (64th bit) that marks a Ticks value as a leap second, i.e., second 60 (one beyond normal second 59).
+    /// </summary>
+    public const ulong LeapSecondFlag = 1UL << 63;
+
+
+    /// <summary>
+    /// Defines the flag (63rd bit) that indicates if leap second is positive or negative; 0 for add, 1 for delete.
+    /// </summary>
+    public const ulong LeapSecondDirection = 1UL << 62;
+
+    /// <summary>
+    /// Defines mask for bits 1 to 62 that make up the value portion of a Ticks that represent time.
+    /// </summary>
+    public const ulong ValueMask = ~LeapSecondFlag & ~LeapSecondDirection;
+
+    /// <summary>
+    /// Defines the Ticks representation of the Unix epoch timestamp starting at January 1, 1970.
+    /// </summary>
+    public const ulong UnixBaseOffset = 621355968000000000;
+
+    /// <summary>
+    /// Gets <see cref="Ticks"/> timestamp for the specified STTP <paramref name="timestamp"/>.
+    /// </summary>
+    /// <param name="timestamp">STTP uint64 timestamp.</param>
+    /// <returns><see cref="Ticks"/> representing specified STTP <paramref name="timestamp"/>.</returns>
+    /// <remarks>
+    /// <see cref="Ticks"/> value can be implicitly cast to and from standard .NET <see cref="DateTime"/>.
+    /// </remarks>
+    public static Ticks ToTicks(ulong timestamp) => new((long)(timestamp & ValueMask));
+
+    /// <summary>
+    /// Gets STTP timestamp for the specified <paramref name="ticks"/>.
+    /// </summary>
+    /// <param name="ticks"><see cref="Ticks"/> timestamp.</param>
+    /// <param name="leapSecond">Flag that determines if <paramref name="ticks"/> represents a leap second.</param>
+    /// <param name="leapSecondIsNegative">Flag that determines if leap second is negative.</param>
+    /// <returns>STTP uint64 timestamp representing specified <paramref name="ticks"/>.</returns>
+    /// <remarks>
+    /// <see cref="Ticks"/> value can be implicitly cast to and from standard .NET <see cref="DateTime"/>.
+    /// </remarks>
+    public static ulong FromTicks(Ticks ticks, bool leapSecond = false, bool leapSecondIsNegative = false)
+    {
+        ulong timestamp = (ulong)ticks.Value;
+
+        if (leapSecond)
+            timestamp |= LeapSecondFlag;
+
+        if (leapSecondIsNegative)
+            timestamp |= LeapSecondDirection;
+
+        return timestamp;
+    }
+}    
---
+++ src/transport/Measurement.cs
@@ -0,0 +1,50 @@
+﻿//******************************************************************************************************
+//  Measurement.cs - Gbtc
+//
+//  Copyright © 2022, Grid Protection Alliance.  All Rights Reserved.
+//
+//  Licensed to the Grid Protection Alliance (GPA) under one or more contributor license agreements. See
+//  the NOTICE file distributed with this work for additional information regarding copyright ownership.
+//  The GPA licenses this file to you under the MIT License (MIT), the "License"; you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://opensource.org/licenses/MIT
+//
+//  Unless agreed to in writing, the subject software distributed under the License is distributed on an
+//  "AS-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. Refer to the
+//  License for the specific language governing permissions and limitations.
+//
+//  Code Modification History:
+//  ----------------------------------------------------------------------------------------------------
+//  09/20/2022 - Ritchie Carroll
+//       Generated original version of source code.
+//
+//******************************************************************************************************
+
+namespace sttp.transport;
+
+/// <summary>
+/// Represents a basic unit of measured data for transmission or reception in the STTP API.
+/// </summary>
+public class Measurement
+{
+    /// <summary>
+    /// Defines measurement's globally unique identifier.
+    /// </summary>
+    public Guid SignalID { get; set; }
+
+    /// <summary>
+    /// Defines instantaneous value of the measurement.
+    /// </summary>
+    public double Value { get; set; }
+
+    /// <summary>
+    /// Defines the STTP uint64 timestamp, in ticks, that measurement was taken.
+    /// </summary>
+    public ulong Timestamp { get; set; }
+
+    /// <summary>
+    /// Defines flags indicating the state of the measurement as reported by the device that took it.
+    /// </summary>
+    public StateFlags Flags { get; set; }
+}
